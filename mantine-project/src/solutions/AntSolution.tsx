ecc16337f0a7887125a6169fed048faf623a45e1
taraswww777 <taraswww777@mail.ru>
2023-04-09 13:56:17 +0300

    make AntSolution

--- mantine-project/src/solutions/AntSolution.tsx
+++ mantine-project/src/solutions/AntSolution.tsx
@@ -1,51 +1,46 @@
 import React, {FC} from 'react';
-import {Badge, Button, Card, Grid, Group, MantineProvider, Mark, Text} from '@mantine/core';
 import {SolutionProps} from "./types";
+import {Layout, Button, Card, Badge, Empty} from "antd";
 
 export const AntSolution: FC<SolutionProps> = ({posts, loadPost, postDetail, loadComments, comments}) => (
-    <MantineProvider>
-        <Grid>
-            <Grid.Col span={12}>
-                {posts.map(({id, title}) => (
-                    <Button
-                        size={"xs"}
-                        variant={postDetail?.id === id ? 'filled' : 'subtle'}
-                        compact
-                        type={'button'}
-                        key={id}
-                        onClick={() => loadPost(id)}>
-                        ID: {id}
+    <Layout>
+        <Layout.Sider theme={"light"}>
+            {posts.map(({id, title}) => (
+                <Button
+                    type={postDetail?.id === id ? 'primary' : 'ghost'}
+                    size={"small"}
+                    shape="round"
+                    htmlType={'button'}
+                    key={id}
+                    onClick={() => loadPost(id)}>
+                    ID: {id}
+                </Button>
+            ))}
+        </Layout.Sider>
+        <Layout.Content>
+            {postDetail?.id ? (
+                <div key={postDetail?.id}>
+                    {postDetail?.body}
+                    <Button onClick={() => loadComments(postDetail?.id)}>
+                        see comment
                     </Button>
-                ))}
-            </Grid.Col>
-            <Grid.Col span={8}>
-                {postDetail?.id ? (
-                    <div key={postDetail?.id}>
-                        {postDetail?.body}
-                        <Button onClick={() => loadComments(postDetail?.id)}>
-                            see comment
-                        </Button>
 
-                        <div>
-                            {comments.map(({id, name, body, email}) => (
-                                <Card key={id} shadow="sm" padding="lg" radius="md" withBorder>
-                                    <Group position="apart" mt="md" mb="xs">
-                                        <Text weight={500}>{name}</Text>
-                                        <Badge color="pink" variant="light">
-                                            {id}
-                                        </Badge>
-                                    </Group>
-                                    <Text size="sm" color="dimmed">
-                                        {body}
-                                    </Text>
+                    <div>
+                        {comments.map(({id, name, body, email}) => (
+                            <Badge.Ribbon
+                                key={id}
+                                text={id}
+                                color="red">
+                                <Card title={name} size="small">
+                                    {body}
                                 </Card>
-                            ))}
-                        </div>
+                            </Badge.Ribbon>
+                        ))}
                     </div>
-                ) : (
-                    <Mark>empty postDetail</Mark>
-                )}
-            </Grid.Col>
-        </Grid>
-    </MantineProvider>
+                </div>
+            ) : (
+                <Empty/>
+            )}
+        </Layout.Content>
+    </Layout>
 )
